0f9a66fa718c8cdc9979bd0cc778ff0453ede7f3
GUMBALL . <xboxrobops2@gmail.com>
2024-12-23 01:40:41 +0700

    Add project files.

---
+++ SushiWebsite/Program.cs
@@ -0,0 +1,36 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Thêm dịch vụ Session
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // Session sống trong 30 phút
+    options.Cookie.HttpOnly = true; // Bảo mật cookie
+    options.Cookie.IsEssential = true; // Cookie này là cần thiết
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseRouting();
+app.UseSession(); // Sử dụng Session
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
